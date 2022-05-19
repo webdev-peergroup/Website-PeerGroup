d415f6061b554c2948a7e96e24c01ce25314b72e
alif ndaru kusuma <alifalip6727@gmail.com>
2022-05-19 21:19:57 +0700

    env alif

--- website-project/app/Config/Database.php
+++ website-project/app/Config/Database.php
@@ -32,15 +32,9 @@ class Database extends Config
      */
     public $default = [
         'DSN'      => '',
-<<<<<<< HEAD
         'hostname' => 'mysql',
         'username' => 'root',
         'password' => 'root',
-=======
-        'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '',
->>>>>>> 1a43174772bde7f38289353f8566f2f8b2e5e038
         'database' => 'peergroupid',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
