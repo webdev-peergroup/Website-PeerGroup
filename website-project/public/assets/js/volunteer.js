7e891bead6d78176848f77b9ee49cd4c774664c6
Stephanie Natasya <tasya315@gmail.com>
2022-05-30 22:30:48 +0700

    Melengkapi page volunteer

--- website-project/public/assets/js/volunteer.js
+++ website-project/public/assets/js/volunteer.js
@@ -1,30 +1,3 @@
-// // Get the modal
-// var modal = document.getElementById("popup");
-
-// // Get the button that opens the modal
-// var btns = document.getElementsByClassName("divisi-pop");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].onclick = function() {
-//     modal.style.display = "block";
-//   }
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
 $(".open_popup").click(function () {
   $(this).parent(".popup_main").children(".popup_body").addClass("popup_body_show");
   });
