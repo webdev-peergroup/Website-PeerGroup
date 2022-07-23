b9f5d03565003224fd56cee82ca6275148b3a541
Stephanie Natasya <tasya315@gmail.com>
2022-07-23 17:46:29 +0700

    Memperbaiki dark mode dan responsive

--- website-project/app/Views/Frontend/artikel/detail-artikel.php
+++ website-project/app/Views/Frontend/artikel/detail-artikel.php
@@ -34,10 +34,10 @@
                         <?= $data['text'] ?>
                     </div>
                     
-                    <div class="animation">
+                    <!-- <div class="animation">
                         <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
                         <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_kmfssxfk.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
-                    </div>
+                    </div> -->
                 </div>
             </article>
             <aside>
