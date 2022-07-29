1a84e2aab261b422ac473b99ea11b99b2d075da8
alif ndaru kusuma <alifalip6727@gmail.com>
2022-07-29 23:29:57 +0700

    env alip

--- website-project/app/Config/App.php
+++ website-project/app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      * @var string
      */
     // public $baseURL = 'https://peergroup.id/';
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'http://webpeergroup.test/';
 
     /**
      * --------------------------------------------------------------------------
